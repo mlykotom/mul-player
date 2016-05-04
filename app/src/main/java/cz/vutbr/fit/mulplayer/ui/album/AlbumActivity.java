3647919b99b82c0fde457ea3ea551eca7cd8c1e0
RomanCizmarik <roman.cizmarik@gmail.com>
2016-05-05 00:32:40 +0200

    Show album cover 1.attempt

--- app/src/main/java/cz/vutbr/fit/mulplayer/ui/album/AlbumActivity.java
+++ app/src/main/java/cz/vutbr/fit/mulplayer/ui/album/AlbumActivity.java
@@ -1,12 +1,22 @@
 package cz.vutbr.fit.mulplayer.ui.album;
 
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cz.vutbr.fit.mulplayer.Constants;
 import cz.vutbr.fit.mulplayer.R;
 import cz.vutbr.fit.mulplayer.ui.BaseActivity;
 import icepick.Icepick;
@@ -16,14 +26,18 @@ import icepick.State;
  * @author mlyko
  * @since 18.04.2016
  */
-public class AlbumActivity extends BaseActivity {
+public class AlbumActivity extends BaseActivity implements Loader.OnLoadCompleteListener<Cursor> {
 	public static final String EXTRA_ALBUM_ID = "album_id";
+	private static final int ALBUM_LOADER_ID = 0;
 
 	AlbumPresenter mPresenter;
 	@Bind(R.id.album_artist) TextView mAlbumArtist;
 	@Bind(R.id.album_name) TextView mAlbumName;
+	@Bind(R.id.album_cover)	ImageView mAlbumCover;
 
 	@State long mAlbumId;
+
+	private CursorLoader mAlbumInfoLoader;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,11 +54,54 @@ public class AlbumActivity extends BaseActivity {
 		}
 
 		mAlbumName.setText(String.valueOf(mAlbumId));
+
+		mAlbumInfoLoader = new CursorLoader(
+				this,
+				MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+				Constants.ALBUMS_PROJECTOR,
+				"_id = ?",
+				new String[]{String.valueOf(mAlbumId)},
+				null
+		);
+		mAlbumInfoLoader.registerListener(ALBUM_LOADER_ID,this);
+
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mAlbumInfoLoader.startLoading();
+
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mAlbumInfoLoader.reset();
 	}
 
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		Icepick.saveInstanceState(this, outState);
+	}
+
+	@Override
+	public void onLoadComplete(Loader<Cursor> loader, Cursor data) {
+		if (loader.getId() == ALBUM_LOADER_ID){
+			if (data != null) {
+				int i = data.getCount();
+				if (data.moveToFirst()){
+
+					String path = data.getString(data.getColumnIndex(MediaStore.Audio.Albums.ALBUM_ART));
+					Drawable img = Drawable.createFromPath(path);
+					mAlbumCover.setImageDrawable(img);
+				}
+
+//				Bitmap bmp= BitmapFactory.decodeFile(path);
+//				mAlbumCover.setImageBitmap(bmp);{
+
+			}
+		}
 	}
 }
