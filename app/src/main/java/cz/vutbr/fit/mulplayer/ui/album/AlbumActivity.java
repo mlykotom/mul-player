d5b9358b8a2aac71b693847dcc98e6b6ba5268e4
RomanCizmarik <roman.cizmarik@gmail.com>
2016-05-06 19:16:07 +0200

    Album detail 2.attempt

--- app/src/main/java/cz/vutbr/fit/mulplayer/ui/album/AlbumActivity.java
+++ app/src/main/java/cz/vutbr/fit/mulplayer/ui/album/AlbumActivity.java
@@ -6,10 +6,15 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,7 +23,9 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import cz.vutbr.fit.mulplayer.Constants;
 import cz.vutbr.fit.mulplayer.R;
+import cz.vutbr.fit.mulplayer.adapter.SongsListAdapter;
 import cz.vutbr.fit.mulplayer.ui.BaseActivity;
+import cz.vutbr.fit.mulplayer.utils.SimpleDividerItemDecoration;
 import icepick.Icepick;
 import icepick.State;
 
@@ -29,15 +36,19 @@ import icepick.State;
 public class AlbumActivity extends BaseActivity implements Loader.OnLoadCompleteListener<Cursor> {
 	public static final String EXTRA_ALBUM_ID = "album_id";
 	private static final int ALBUM_LOADER_ID = 0;
+	private static final int SONGS_LOADER_ID = 1;
 
 	AlbumPresenter mPresenter;
-	@Bind(R.id.album_artist) TextView mAlbumArtist;
-	@Bind(R.id.album_name) TextView mAlbumName;
+
 	@Bind(R.id.album_cover)	ImageView mAlbumCover;
+	@Bind(R.id.album_songs_list)	RecyclerView mAlbumSongsList;
 
 	@State long mAlbumId;
 
 	private CursorLoader mAlbumInfoLoader;
+	private CursorLoader mSongsLoader;
+
+	private SongsListAdapter mSongsListAdapter;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,8 +64,7 @@ public class AlbumActivity extends BaseActivity implements Loader.OnLoadComplete
 			mAlbumId = intent.getLongExtra(EXTRA_ALBUM_ID, 0);
 		}
 
-		mAlbumName.setText(String.valueOf(mAlbumId));
-
+		// album loader, loads info about this album
 		mAlbumInfoLoader = new CursorLoader(
 				this,
 				MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
@@ -65,12 +75,33 @@ public class AlbumActivity extends BaseActivity implements Loader.OnLoadComplete
 		);
 		mAlbumInfoLoader.registerListener(ALBUM_LOADER_ID,this);
 
+		//loading songs from this album
+		mSongsLoader = new CursorLoader(
+				this,
+				MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+				Constants.SONG_PROJECTOR,
+				"album_id = ?",
+				new String[]{String.valueOf(mAlbumId)},
+				null
+		);
+
+		mSongsLoader.registerListener(SONGS_LOADER_ID, this);
+
+		// showing songs from this album
+		mSongsListAdapter = new SongsListAdapter(this, Constants.SONG_PROJECTOR);
+
+		//wtf je toto
+		mAlbumSongsList.setLayoutManager(new LinearLayoutManager(this));
+		mAlbumSongsList.setItemAnimator(new DefaultItemAnimator());
+		mAlbumSongsList.setAdapter(mSongsListAdapter);
+		mAlbumSongsList.addItemDecoration(new SimpleDividerItemDecoration(this));
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		mAlbumInfoLoader.startLoading();
+		mSongsLoader.startLoading();
 
 	}
 
@@ -95,13 +126,19 @@ public class AlbumActivity extends BaseActivity implements Loader.OnLoadComplete
 
 					String path = data.getString(data.getColumnIndex(MediaStore.Audio.Albums.ALBUM_ART));
 					Drawable img = Drawable.createFromPath(path);
+
+					ColorMatrix matrix = new ColorMatrix();
+					matrix.setSaturation(0);
+
+					ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+					mAlbumCover.setColorFilter(filter);
 					mAlbumCover.setImageDrawable(img);
 				}
-
-//				Bitmap bmp= BitmapFactory.decodeFile(path);
-//				mAlbumCover.setImageBitmap(bmp);{
-
 			}
+		}
+
+		if (loader.getId() == SONGS_LOADER_ID){
+			mSongsListAdapter.swapCursor(data);
 		}
 	}
 }
